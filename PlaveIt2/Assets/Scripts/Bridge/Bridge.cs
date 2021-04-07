d8001e3ffe81bdc00110fe2b03516a4a0b64c1f6
Furanimus <babitskyd@gmail.com>
2021-04-07 17:34:09 +0300

    DragObject
    Deleted some bridges.
    Added custom bridge and attached drag script.

--- PlaveIt2/Assets/Scripts/Bridge/Bridge.cs
+++ PlaveIt2/Assets/Scripts/Bridge/Bridge.cs
@@ -8,6 +8,10 @@ public class Bridge : MonoBehaviour
     public int spacesOccupies = 2;
     public int id;
 
+    public Transform bridge;
+    private float bridgeAngle = 5f;
+    public float bridgeRotationSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,7 @@ public class Bridge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateOnClickAndDrag();
     }
 
     private void initBridge()
@@ -26,6 +30,11 @@ public class Bridge : MonoBehaviour
 
     private void RotateOnClickAndDrag()
     {
-
+        bridgeAngle += Input.GetAxis("Mouse X") * bridgeRotationSpeed * -Time.deltaTime;
+        bridgeAngle = Mathf.Clamp(bridgeAngle, 0, 180);
+        //if (Input.GetMouseButtonDown(0));
+        //{
+        //    this.transform.Rotate(Time.deltaTime * 10);
+        //}
     }
 }
