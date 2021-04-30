09e83b684b551525bff13e7b65d32ccff9670dc8
Furanimus <babitskyd@gmail.com>
2021-04-30 16:37:09 +0300

    fixed fragBridge

--- PlaveIt2/Assets/Scripts/Bridge/DragBridge.cs
+++ PlaveIt2/Assets/Scripts/Bridge/DragBridge.cs
@@ -3,78 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragObject : MonoBehaviour
+public class DragBridge : MonoBehaviour
 {
-    Vector3 mouseAsWorldPosition;
-    Vector3 objectPosition;
-    private float mousePositionX;
-    private float mousePositionY;
-    private float mousePositionZ;
-    
-    void OnMouseUp()
-    {
-        HoverHighlight HoverHighlightScriptReference = GetComponent<HoverHighlight>();
-        HoverHighlightScriptReference.ResetColor();
-    }
+	[SerializeField] private float xVelocity = 7f;
+	[SerializeField] private float zVelocity = 10f;
 
-    void OnMouseDown()
-    {
-        objectPosition = transform.position;
-        mouseAsWorldPosition = Camera.main.WorldToScreenPoint(transform.position);
-        mousePositionX = Input.mousePosition.x - mouseAsWorldPosition.x;
-        mousePositionZ = Input.mousePosition.y - mouseAsWorldPosition.y;
-        mousePositionY = Input.mousePosition.z - mouseAsWorldPosition.z;
-    }
+	Vector3 mouseAsWorldPosition;
+	Vector3 objectPosition;
+	private float mouseAxisX;
+	private float mousePositionY;
+	private float mouseAxisY;
+	private static bool isClicked = false;
 
-    void OnMouseDrag()
-    {
-        float distanceX = Input.mousePosition.x - mousePositionX;
-        float distanceY = Input.mousePosition.y - mousePositionZ;
-        float distanceZ = Input.mousePosition.z - mousePositionY;
-        Vector3 finalPosition = Camera.main.ScreenToWorldPoint(new Vector3(distanceX, distanceY, distanceZ));
-        transform.position = new Vector3(finalPosition.x, objectPosition.y, finalPosition.z);
-    }
+	void OnMouseUp()
+	{
+		Cursor.visible = true;
+		isClicked = false;
+		HoverHighlight HoverHighlightScriptReference = GetComponent<HoverHighlight>();
+		HoverHighlightScriptReference.ResetColor();
+	}
 
+	void OnMouseDown()
+	{
+		Cursor.visible = false;
+		isClicked = true;
+	}
 
-    void OnMouseOver()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            //do stuff here
-            transform.rotation *= Quaternion.Euler(0, 0, 90f);
-        }
-    }
+	void OnMouseDrag()
+	{
+		mouseAxisX = Input.GetAxis("Mouse X");
+		mouseAxisY = Input.GetAxis("Mouse Y");
+
+		transform.position += new Vector3(mouseAxisX * xVelocity, 0, mouseAxisY * zVelocity);
+		if (Input.GetMouseButtonDown(1))
+		{
+			Rotate90Degree();
+		}
+	}
+
+	void OnMouseOver()
+	{
+		if (!isClicked && Input.GetMouseButtonDown(1))
+		{
+			Rotate90Degree();
+		}
+	}
+
+	private void Rotate90Degree()
+	{
+		transform.rotation *= Quaternion.Euler(0, 0, 90f);
+	}
 }
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class DragObject : MonoBehaviour
-//{
-//    private Vector3 mOffset;
-//    private float mZCoord;
-
-//    void OnMouseDown()
-//    {
-//        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-//        // Store offset = gameobject world pos - mouse world pos
-//        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-//    }
-
-//    private Vector3 GetMouseAsWorldPoint()
-//    {
-//        // Pixel coordinates of mouse (x,y)
-//        Vector3 mousePoint = Input.mousePosition;
-//        // z coordinate of game object on screen
-//        mousePoint.z = mZCoord;
-//        // Convert it to world points
-//        return Camera.main.ScreenToWorldPoint(mousePoint);
-//    }
-
-//    void OnMouseDrag()
-//    {
-//        transform.position = GetMouseAsWorldPoint() + mOffset;
-//    }
-//}
