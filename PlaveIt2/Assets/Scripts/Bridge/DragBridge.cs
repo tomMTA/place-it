76e28f7a8c10c34cfdb0d73742b23f1033d7c2a1
Tom Weinberg <tomwe@mta.ac.il>
2021-04-30 18:17:30 +0300

    Updated DragBridge with boundaries
    Limited the bridge's movement to 100 distance from the center of the GameBoard.
    Moved camera in scene a little bit back so the bridge won't get out of view

--- PlaveIt2/Assets/Scripts/Bridge/DragBridge.cs
+++ PlaveIt2/Assets/Scripts/Bridge/DragBridge.cs
@@ -1,5 +1,4 @@
 ﻿
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -16,6 +15,22 @@ public class DragBridge : MonoBehaviour
 	private float mousePositionY;
 	private float mouseAxisY;
 	private static bool isClicked = false;
+	private const float rangeFromBoard = 100;
+	private GameObject gameBoard; //redundant to hold as member? maybe just create to exctract the Vector3?
+	private Vector3 boardPosition; //redundant to hold as member? maybe just create to exctract the Vector3?
+	private float[] xBounadries = new float[2];
+	private float[] zBoundaries = new float[2];
+
+	void Start()
+    {
+		gameBoard = GameObject.Find("GameBoard");
+		MeshRenderer boardRenderer = gameBoard.GetComponentsInChildren<MeshRenderer>()[0]; //if we add more renderers we need to modify this
+		boardPosition = boardRenderer.bounds.center;
+		xBounadries[0] = boardPosition.x - rangeFromBoard;
+		xBounadries[1] = boardPosition.x + rangeFromBoard;
+		zBoundaries[0] = boardPosition.z - rangeFromBoard;
+		zBoundaries[1] = boardPosition.z + rangeFromBoard;
+	}
 
 	void OnMouseUp()
 	{
@@ -35,8 +50,19 @@ public class DragBridge : MonoBehaviour
 	{
 		mouseAxisX = Input.GetAxis("Mouse X");
 		mouseAxisY = Input.GetAxis("Mouse Y");
+		float newX = transform.position.x + mouseAxisX * xVelocity;
+		float newZ = transform.position.z + mouseAxisY * zVelocity;
 
-		transform.position += new Vector3(mouseAxisX * xVelocity, 0, mouseAxisY * zVelocity);
+		if (!isInXRange(newX))
+		{
+			newX = transform.position.x;
+		}
+		if (!isInZRange(newZ))
+		{
+			newZ = transform.position.z;
+		}
+		transform.position = new Vector3(newX, transform.position.y, newZ);
+
 		if (Input.GetMouseButtonDown(1))
 		{
 			Rotate90Degree();
@@ -54,5 +80,16 @@ public class DragBridge : MonoBehaviour
 	private void Rotate90Degree()
 	{
 		transform.rotation *= Quaternion.Euler(0, 0, 90f);
+	}
+
+	//improve performance - calculate posToVerify once
+	private bool isInXRange(float newX)
+    {
+		return (newX >= xBounadries[0]) && (newX <= xBounadries[1]);
+	}
+
+	private bool isInZRange(float newZ)
+	{
+		return (newZ >= zBoundaries[0]) && (newZ <= zBoundaries[1]);
 	}
 }
