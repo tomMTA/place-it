06a6295aeb2ac4913ef8cf62875d23b620b6eea7
Furanimus <babitskyd@gmail.com>
2021-05-02 16:53:02 +0300

    Added rotation to board

--- PlaveIt2/Assets/Scripts/Board/BoardRotate.cs
+++ PlaveIt2/Assets/Scripts/Board/BoardRotate.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class BoardRotate : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 10;
-    private float mouseAxisX;
+    //[SerializeField] private float rotationSpeed = 50f;
+    private float rotationSpeed = 500f;
+    private const string BoardPivotPointName = "BoardPivotPoint";
+    private GameObject BoardPivotPoint;
+    [SerializeField] private GameObject _default;
+    private float rotationZ;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+	    BoardPivotPoint = GameObject.Find(BoardPivotPointName);
     }
 
     void OnMouseOver()
@@ -22,22 +27,31 @@ public class BoardRotate : MonoBehaviour
 
     void OnMouseDown()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     void OnMouseUp()
     {
-        Cursor.visible = true;
+        //Cursor.visible = true;
     }
 
     void OnMouseDrag()
     {
-        transform.Rotate(0, 0, -(Input.GetAxis("Mouse X") * rotationSpeed));
+	    float newAngle = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+		_default.transform.RotateAround(BoardPivotPoint.transform.position, Vector3.down, newAngle);
     }
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
-        
-    }
+	    if (Input.GetKeyDown(KeyCode.B))
+	    {
+		    GameObject.Find("BlueBridge1").transform.parent = this.transform;
+	    }
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			Debug.Log("transform.rotation angles x: " + _default.transform.eulerAngles.x + " y: " + _default.transform.eulerAngles.y + " z: " + _default.transform.eulerAngles.z);
+			Debug.Log(Input.GetAxis("Mouse X"));
+		}
+	}
 }
