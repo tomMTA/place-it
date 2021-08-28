6ab9c6e857a482a92ba260ae05df6dafc9b0706b
Furanimus <babitskyd@gmail.com>
2021-08-28 17:21:23 +0300

    started UI panel

---
+++ PlaveIt2/Assets/Scripts/UI/IToolbarBridge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public interface IToolbarBridge
+{
+	string Name { get; }
+
+	Sprite Image { get; }
+
+	void OnReturn();
+
+	void OnDrop();
+}
+
+public class ToolbarEventArgs : EventArgs
+{
+	IToolbarBridge Bridge;
+
+	public ToolbarEventArgs (IToolbarBridge bridge)
+	{
+		Bridge = bridge;
+	}
+}
