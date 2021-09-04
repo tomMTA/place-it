cf7dcb6ebbd9afc4ce2ebbcff94e188a59092cd1
Tom Weinberg <58363976+tomMTA@users.noreply.github.com>
2021-09-04 11:45:03 +0300

    Added timer
    -added timer to all level
    -added eventSystem to all levels

---
+++ PLACE-IT/Assets/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] public Text timerText;
+    private float m_StartTime;
+    private int m_CurrentMinutes;
+    private int m_CurrentSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - m_StartTime;
+        m_CurrentMinutes = (int)t / 60;
+        m_CurrentSeconds = (int)t % 60;
+
+        timerText.text = "";
+        if (m_CurrentMinutes < 10)
+        {
+            timerText.text += "0";
+        }
+
+        timerText.text += m_CurrentMinutes.ToString() + ":";
+        if (m_CurrentSeconds < 10)
+        {
+            timerText.text += "0";
+        }
+
+        timerText.text += m_CurrentSeconds.ToString();
+    }
+
+    public int Seconds
+    {
+        get { return m_CurrentMinutes * 60 + m_CurrentSeconds; }
+    }
+
+}
