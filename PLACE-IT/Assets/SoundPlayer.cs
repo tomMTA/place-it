6edcc401d4f0c96dfbaeb4ed9a0a3d2d0735df68
Tom Weinberg <58363976+tomMTA@users.noreply.github.com>
2021-09-07 20:22:22 +0300

    Music

--- PLACE-IT/Assets/SoundPlayer.cs
+++ PLACE-IT/Assets/SoundPlayer.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource[] m_AudioSource;
+    [SerializeField] private Slider m_MusicSlider;
+    [SerializeField] private Slider m_EffectsSlider;
+
+    void Start()
+    {
+        //SetEffectsVolume();
+        //SetMusicVolume();
+        //PlayThemeSound();
+    }
 
     public void PlayClickSound()
     {
@@ -13,5 +23,38 @@ public class SoundPlayer : MonoBehaviour
     public void PlayWinSound()
     {
         m_AudioSource[1].Play();
+    }
+
+    /*public void PlayThemeSound()
+    {
+        m_AudioSource[2].Play();
+    }*/
+
+    public void ToggleEffects()
+    {
+        m_AudioSource[0].mute = !m_AudioSource[0].mute;
+        m_AudioSource[1].mute = !m_AudioSource[1].mute;
+    }
+
+    /*public void ToggleMusic()
+    {
+        m_AudioSource[2].mute = !m_AudioSource[2].mute;
+    }*/
+
+    public void SetEffectsVolume(float i_Value)
+    {
+        m_AudioSource[0].volume = i_Value;
+        m_AudioSource[1].volume = i_Value;
+    }
+
+    /*public void SetEffectsVolume()
+    {
+        m_AudioSource[0].volume = m_EffectsSlider.value;
+        m_AudioSource[1].volume = m_EffectsSlider.value;
+    }*/
+
+    public void SetMusicVolume(Slider i_Slider)
+    {
+        GameObject.FindWithTag("ThemeSound").GetComponent<AudioSource>().volume = i_Slider.value;
     }
 }
